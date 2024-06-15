b0391ceedf91846261d81ebd8b7cee1bdb5cb549
Abhay Tiwari <abhaytiwari713@gmail.com>
2024-06-15 17:32:19 +0530

    add lsp config.

--- lua/abhay/lazy.lua
+++ lua/abhay/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("abhay.plugins", {
+require("lazy").setup({{import = "abhay.plugins"}, {import = "abhay.plugins.lsp"}}, {
   change_detection = {
     checker = {
       enabled = true,
