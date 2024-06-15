08880ea493aa058be5da99eb071f4427d4fadad2
Abhay Tiwari <abhaytiwari713@gmail.com>
2024-06-15 17:45:43 +0530

    added linting functionality

--- lua/abhay/plugins/lsp/mason.lua
+++ lua/abhay/plugins/lsp/mason.lua
@@ -35,10 +35,12 @@ return {
 
 		mason_tool_installer.setup({
 			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
+				"eslint_d",
+				"pylint",
 			},
 		})
 	end,
